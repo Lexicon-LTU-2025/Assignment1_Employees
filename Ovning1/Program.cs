548daf45c51ebe7c2225d5b61cf6c30b099a366b
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-22 12:07:38 +0200

    Add project files.

---
+++ Ovning1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ovning1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("Kalle", 2);
+            Robot robot2 = new Robot();
+            string robotName = robot.GetName();
+            robot.Age = 20;
+            // robot.SomeField = 30;
+            //robot.Salary = 50;
+            
+          
+        }
+    }
+}
---
+++ Ovning1/Robot.cs
@@ -0,0 +1,52 @@
+﻿namespace Ovning1;
+
+internal class Robot
+{
+    private string _name;
+
+    private int _age;
+
+    public int Age
+    {
+        get { return _age; }
+        set 
+        {
+            if (value < 0)
+            {
+                _age = 0;
+            }
+            else
+            {
+              _age = value; 
+            }
+        
+        }
+    }
+
+      public int Salary { get; private set; }
+
+    //  public int SomeField;
+
+    public Robot(string name, int age)
+    {
+        _name = name;
+        Age = age;
+    }
+
+    public Robot()
+    {
+            
+    }
+
+    public string GetName()
+    {
+       // List<int> list = new();
+        return _name;
+    }
+
+    public void SetName(string value)
+    {
+        //Validate
+        _name = value;
+    }
+}
