7bea33cd4757a91969c5afb8f22793a2968e3ebf
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-22 13:23:43 +0200

    Change program

--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -5,7 +5,6 @@
         static void Main(string[] args)
         {
             Robot robot = new Robot("Kalle", 2);
-            Robot robot2 = new Robot();
             string robotName = robot.GetName();
             robot.Age = 20;
             // robot.SomeField = 30;
