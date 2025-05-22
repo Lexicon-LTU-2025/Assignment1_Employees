e931344da8176b9c2baeb0d2003f0c63b3255ca1
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-22 13:25:12 +0200

    Remove program junk

--- Ovning1/Program.cs
+++ Ovning1/Program.cs
@@ -4,13 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Robot robot = new Robot("Kalle", 2);
-            string robotName = robot.GetName();
-            robot.Age = 20;
-            // robot.SomeField = 30;
-            //robot.Salary = 50;
-            
-          
+           
         }
     }
 }
